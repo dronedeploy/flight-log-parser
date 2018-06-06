156b3531131db517fedf1b664acf8be7caf0c992
sonrisacc <sonrisa@dronedeploy.com>
2018-06-06 09:08:16 -0700

    Setup jest

---
+++ src/parser.test.ts
@@ -0,0 +1,21 @@
+import 'jest';
+// import path from 'path';
+// const path = require( 'path');
+// const parseLog  = require('./parser.ts');
+// let parsedLog;
+// beforeAll(() => {
+//   const filePath = path.join(__dirname, '../testlog/ios11-phantom4.log');
+//   console.log("filePath", filePath);
+//   parseLog(filePath).then((text) => {
+//       console.log("text", text);
+//       parsedLog = text;
+//   });
+//   return parsedLog;
+// });
+
+
+describe('Users', () => {
+    it('adds 1 + 2 to equal 3 in JavaScript', ()=> {
+      expect(3).toBe(3);
+    });
+});
--- src/parser.ts
+++ src/parser.ts
@@ -156,4 +156,3 @@ function parseMetaData(headers: string[], footers: string[]): FlightLogMetaData 
     },
   };
 };
-

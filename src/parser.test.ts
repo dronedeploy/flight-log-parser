19fdebe422102318b9fbfa53be5cf7fab2b67d96
sonrisacc <sonrisa@dronedeploy.com>
2018-06-07 15:57:50 -0700

    Update test structure

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -2,18 +2,20 @@ import 'jest';
 import path from 'path';
 import fs from 'fs';
 import {promisify} from 'util';
-
 import { parseLog } from './parser';
 
 const readFileAsync = promisify(fs.readFile);
-const iosfilePath = path.join(__dirname, '../testlog/android.log');
-const androidFilePath = path.join(__dirname, '../testlog/ios11-2.log');
+
+// Maybe we can read all files from one folder?
+const iosFilePath = path.join(__dirname, '../testlog/ipad-ios11-phantom4.log');
+const androidFilePath = path.join(__dirname, '../testlog/pixel2-inspire2.log');
+// const iosFilePath2 = path.join(__dirname, '../testlog/iphone-ios11-inspire.log');
 
 describe('test parse logs', () => {
     let parsedLogs : any;
     beforeAll(async() => {
         const testLogs = {
-            ios: await readFileAsync(iosfilePath, {encoding: 'utf8'}),
+            ios: await readFileAsync(iosFilePath, {encoding: 'utf8'}),
             android: await readFileAsync(androidFilePath, {encoding: 'utf8'}),
             // other: await readFileAsync(otherPath, {encoding: 'utf8'}),
         };
@@ -22,14 +24,28 @@ describe('test parse logs', () => {
     });
 
     it('parsed log should exist', async() => {
-      console.log("parsedLogs", parsedLogs[0].metaData);
+      // console.log("parsedLogs", parsedLogs[0].metaData);
       expect(parsedLogs).toBeTruthy();
     });
 
-    it('parsed log should have metaData and rows', async() => {
-      const result = parsedLogs.filter((cur) => !!cur.metaData && !!cur.rows);
-      expect(result.length).toBe(parsedLogs.length);
+    describe('test ios logs', () => {
+        it('parsed log should have correct metaData', async() => {
+            const ioslogMetaData = parsedLogs[0].metaData;
+            const os = ioslogMetaData.device.os;
+            const re = /(ios \d+(\.\d)*)/i
+            const result = os.match(re);
+            expect(result).toBeTruthy();
+        });
     });
 
-    // it('should ')
+    describe('test android logs', () => {
+        it('parsed log should have correct metaData', async() => {
+            const ioslogMetaData = parsedLogs[1].metaData;
+            const os = ioslogMetaData.device.os;
+            const re = /(android \d+(\.\d)*)/i
+            const result = os.match(re);
+            expect(result).toBeTruthy();
+        });
+    });
 });
+    // it('should ')

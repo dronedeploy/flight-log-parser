aeee9e40f3ce431feee0d330c2e07d84dac38ee1
sonrisacc <sonrisa@dronedeploy.com>
2018-06-07 14:13:47 -0700

    Update init setup

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -1,21 +1,33 @@
 import 'jest';
-// import path from 'path';
-// const path = require( 'path');
-// const parseLog  = require('./parser.ts');
-// let parsedLog;
-// beforeAll(() => {
-//   const filePath = path.join(__dirname, '../testlog/ios11-phantom4.log');
-//   console.log("filePath", filePath);
-//   parseLog(filePath).then((text) => {
-//       console.log("text", text);
-//       parsedLog = text;
-//   });
-//   return parsedLog;
-// });
+import path from 'path';
+import fs from 'fs';
+import {promisify} from 'util';
 
+import { parseLog } from './parser';
+
+const readFileAsync = promisify(fs.readFile);
+const iosfilePath = path.join(__dirname, '../testlog/android.log');
+const androidFilePath = path.join(__dirname, '../testlog/ios11-2.log');
+
+let parsedLog : any;
+beforeAll(() => {
+    readFileAsync(iosfilePath, {encoding: 'utf8'})
+      .then((text) => {
+          parseLog(text).then((result) => {
+              // console.log('result:', result);
+              parsedLog = result;
+              return parsedLog;
+          });
+      })
+      .catch((err) => {
+          console.log('ERROR:', err);
+      });
+
+});
 
 describe('Users', () => {
     it('adds 1 + 2 to equal 3 in JavaScript', ()=> {
       expect(3).toBe(3);
+      // expect(parsedLog).toBeTruthy();
     });
 });

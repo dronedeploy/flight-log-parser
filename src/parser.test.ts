c4ebc0a53a389002668e1201c1ae144f7a391ef1
sonrisacc <sonrisa@dronedeploy.com>
2018-06-07 14:40:46 -0700

    Update raw init setup

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -9,25 +9,18 @@ const readFileAsync = promisify(fs.readFile);
 const iosfilePath = path.join(__dirname, '../testlog/android.log');
 const androidFilePath = path.join(__dirname, '../testlog/ios11-2.log');
 
-let parsedLog : any;
-beforeAll(() => {
-    readFileAsync(iosfilePath, {encoding: 'utf8'})
-      .then((text) => {
-          parseLog(text).then((result) => {
-              // console.log('result:', result);
-              parsedLog = result;
-              return parsedLog;
-          });
-      })
-      .catch((err) => {
-          console.log('ERROR:', err);
-      });
+describe('test parse logs', () => {
+    let testLogs : any;
+    beforeAll(async() => {
+        testLogs = {
+            ios: await readFileAsync(iosfilePath, {encoding: 'utf8'}),
+            android: await readFileAsync(androidFilePath, {encoding: 'utf8'}),
+            // other: await readFileAsync(otherPath, {encoding: 'utf8'}),
+        };
+    });
 
-});
-
-describe('Users', () => {
-    it('adds 1 + 2 to equal 3 in JavaScript', ()=> {
-      expect(3).toBe(3);
-      // expect(parsedLog).toBeTruthy();
+    it('parsed log should exist', async() => {
+      const result = await parseLog(testLogs.ios);
+      expect(result).toBeTruthy();
     });
 });

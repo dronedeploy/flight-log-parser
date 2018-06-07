c154071320bd1c5244d1db550a3f4f2ae2bee949
sonrisacc <sonrisa@dronedeploy.com>
2018-06-07 14:57:14 -0700

    Update dummy test

--- src/parser.test.ts
+++ src/parser.test.ts
@@ -10,17 +10,24 @@ const iosfilePath = path.join(__dirname, '../testlog/android.log');
 const androidFilePath = path.join(__dirname, '../testlog/ios11-2.log');
 
 describe('test parse logs', () => {
-    let testLogs : any;
+    let parsedLogs : any;
     beforeAll(async() => {
-        testLogs = {
+        const testLogs = {
             ios: await readFileAsync(iosfilePath, {encoding: 'utf8'}),
             android: await readFileAsync(androidFilePath, {encoding: 'utf8'}),
             // other: await readFileAsync(otherPath, {encoding: 'utf8'}),
         };
+        const logs = Object.values(testLogs).map(cur => parseLog(cur));
+        parsedLogs = await Promise.all(logs);
     });
 
     it('parsed log should exist', async() => {
-      const result = await parseLog(testLogs.ios);
-      expect(result).toBeTruthy();
+      expect(parsedLogs).toBeTruthy();
     });
+
+    it('parsed log should have metaData and rows', async() => {
+      const result = [parsedLogs].filter((cur) => !!cur.metaData && !!cur.rows);
+      expect(result.length).toBe(parsedLogs.length);
+    });
+
 });
